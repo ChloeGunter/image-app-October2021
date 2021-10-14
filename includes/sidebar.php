ce37add4cac95028c9694c52ef5b7a43c9483e55
Chloe Gunter <85974763+ChloeGunter@users.noreply.github.com>
2021-10-14 11:52:39 -0700

    * "chuncked" the header, sidebar and footer * made the dates pretty * added the list of users, categories and tags to the sidebar

---
+++ includes/sidebar.php
@@ -0,0 +1,69 @@
+<aside class="sidebar">
+	
+	<?php //get up to 5 users, newest first
+	$result = $DB->prepare('SELECT profile_pic, username
+							FROM users
+							ORDER BY user_id DESC
+							LIMIT 5');
+	//run it.
+	$result->execute();
+	//check it.
+	if( $result->rowCount() >= 1 ){
+	 ?>
+	<section class="users">
+		<h3>Newest Users</h3>
+		<ul>
+			<?php while( $row = $result->fetch() ){ ?>
+			<li class="user">
+				<img src="<?php echo $row['profile_pic']; ?>" width="50" height="50" />
+				<?php echo $row['username']; ?>
+			</li>
+			<?php } ?>
+		</ul>
+	</section>
+	<?php } ?>
+
+
+	<?php 
+	//get up to 20 categories in alphabetical order by name
+	$result = $DB->prepare('SELECT name
+							FROM categories
+							ORDER BY category_id ASC
+							LIMIT 20');
+	//run it.
+	$result->execute();
+	//check it.
+	if( $result->rowCount() >= 1 ){
+	 ?>
+	<section class="categories">
+		<h3>Categories</h3>
+		<ul>
+			<?php while( $row = $result->fetch() ){ ?>
+			<li><?php echo $row['name']; ?> - X posts</li>
+			<?php } ?>
+		</ul>
+	</section>
+	<?php } ?>
+
+	<?php 
+	//get up to 20 tags in alphabetical order by name
+	$result = $DB->prepare('SELECT name
+							FROM tags
+							ORDER BY tag_id ASC
+							LIMIT 20');
+	//run it.
+	$result->execute();
+	//check it.
+	if( $result->rowCount() >=1 ){
+	 ?>
+	<section class="tags">
+		<h3>Tags</h3>
+		<ul>
+			<?php while( $row = $result->fetch() ){ ?>
+			<li><?php echo $row['name']; ?> - X posts</li>
+			<?php } ?>
+		</ul>
+	</section>
+	<?php } ?>
+
+</aside>
